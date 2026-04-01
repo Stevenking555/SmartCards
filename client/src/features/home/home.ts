d7a51c489e4213a892231943c3386cb32e40b608
Istvan Laczko <laczkopisti55@gmail.com>
2026-04-01 22:36:19 +0200

    ngOnInit issues fixed

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { DeckService } from '../../core/services/deck.service';
@@ -8,7 +8,6 @@ import { BottomNavComponent } from '../../layout/bottom-nav/bottom-nav';
 import { AccountService } from '../../core/services/account-service';
 import { LanguageButtonComponent } from '../../shared/components/language-button/language-button';
 import { ThemeButtonComponent } from '../../shared/components/theme-button/theme-button';
-
 
 @Component({
   selector: 'app-home',
@@ -25,16 +24,11 @@ export class HomeComponent implements OnInit {
   totalDecks = 0;
   cardsDueToday = 0;
   totalCardsFlipped = 1420; // Just some example TODO: Make it real with real DB data
-  username = '';
+  username = computed(() => this.accountService.currentUser()?.displayName || 'Guest');
 
 
   ngOnInit() {
-    const user = this.accountService.currentUser();
-    if (user) {
-      this.username = user.displayName;
-    }
-
-    this.deckService.loadDecks().subscribe();
+    // this.deckService.loadDecks().subscribe();
     this.deckService.decks$.subscribe(decks => {
       this.totalDecks = decks.length;
       this.cardsDueToday = decks.reduce((sum, deck) => sum + deck.due, 0);
